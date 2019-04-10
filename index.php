62fe307896a170c9349999b5d99cb6fc0582cb67
champyoon <42302061+champyoon@users.noreply.github.com>
2019-04-10 11:10:04 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>ALDP Homepage</title>
+    <link rel="stylesheet" href="CSS\cascadeStyleSheet.css">
+</head>
+
+<body>
+	<form action="main.php" style="margin-top:50px; float:left">
+		<fieldset>
+			<legend>Personal Information</legend>
+			User name<br>
+			<input type="text" name="user"><br>
+			Password<br>
+			<input type="text" name="pass"><br>
+			<input type="submit" value="Sign in" style="margin-top:50px;float:right">
+		</fieldset>
+	</form>
+		
+	<img src="picture\galaxy.jpg" width=1000px style="float:left">
+</body>
+
+</html>
